12e872fd4bd15878185e0e72e8ee7b36c5e0ae15
Aaron Curry <aaron@aaroncurry.net>
2021-02-06 13:17:21 -0600

    Get basic PDF generation going

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 The goal is to have a lightweight app that I can generate printable PDFs of coin inserts.
 
 ## Todo
-- Make countries editable
 - Generate printable PDFs
 
 # Running the webapp
--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 
 require "sinatra"
 require "sinatra/activerecord"
-# require "models/country"
+require "prawn"
+require "prawn/measurement_extensions"
 
 set :database, {adapter: "sqlite3", database: "db/coin_inserts.sqlite3"}
 
@@ -14,5 +15,52 @@ end
 
 post '/add_country' do
   Country.create!(params)
+  redirect to('/')
+end
+
+post '/update_country' do
+  country = Country.find(params[:id])
+  if params[:do] =='Update'
+    country.update!(name: params[:name], years: params[:years])
+  elsif params[:do] =='Delete'
+    country.destroy
+  end
+
+  redirect to('/')
+end
+
+get "/generate" do
+
+  Prawn::Document.generate("Coin inserts.pdf", left_margin: 0.25.in, bottom_margin: 0.5.in) do
+
+    Country.all.each_slice(20) do |countries|
+
+      puts "countries #{countries}"
+
+      stroke do
+        # Each page is a grid of 4w, 5h 2x2 squares
+        columns = 4
+        rows = 5
+        width = height = 2.in
+
+        columns.times do |column|
+          x = column * width
+          rows.times do |row|
+            idx = column + row
+            country = countries[idx]
+
+            if country
+              text country.name
+            end
+
+            y = 10.in - row * height
+            rectangle [x, y], width, height
+          end
+        end
+      end
+
+    end
+  end
+
   redirect to('/')
 end
